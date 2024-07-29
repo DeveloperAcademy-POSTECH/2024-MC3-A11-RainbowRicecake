22d4bcdd5f48b5380581ee6d978c0c46b931ef90
norang <norang1221@gmail.com>
2024-07-29 14:16:33 +0900

    [View] ContentView 에 TabView 추가, 세 가지 메인탭 만들었습니다.

---
+++ YouTalkAwesome/View/ArchiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchiveView.swift
+//  YouTalkAwesome
+//
+//  Created by Kyuhee hong on 7/29/24.
+//
+
+import SwiftUI
+
+struct ArchiveView: View {
+    var body: some View {
+        Text("Hello, ArchiveView!")
+    }
+}
+
+#Preview {
+    ArchiveView()
+}
--- YouTalkAwesome/View/ContentView.swift
+++ YouTalkAwesome/View/ContentView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(systemName: "globe")
+        TabView {
+            LogicalStructureView()
+                .tabItem {
+                    Label("논리 구조", systemImage: "text.bubble")
+                }
+            ScriptWritingView()
+                .tabItem {
+                    Label("대본 작성", systemImage: "doc.append")
+                }
+            ArchiveView()
+                .tabItem {
+                    Label("저장소", systemImage: "person.crop.circle.fill")
+                }
+        }
     }
 }
 
