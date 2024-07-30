b2170a172347cbeee02845b1b378246c2b184961
norang <norang1221@gmail.com>
2024-07-30 21:55:30 +0900

    NavigationStack 중복 제거

--- YouTalkAwesome/View/SpeackingStructure/StructureFlowView.swift
+++ YouTalkAwesome/View/SpeackingStructure/StructureFlowView.swift
@@ -13,48 +13,46 @@ struct StructureFlowView: View {
     @State var showExampleContents: Bool = false
     
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading) {
-                LSWideCardView(speakingStructure: speakingStructure)
-                
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 0) {
-                        ForEach(0..<speakingStructure.components.count, id: \.self) { index in
-                            FlowComponentView(speakingStructure: speakingStructure, index: index, showExampleContents: $showExampleContents)
-                        }
-                    }
-                    .padding()
-                    // 예시문장 열고 닫는 버튼
-                    Button(action: {
-                        withAnimation {
-                            showExampleContents.toggle()
-                        }
-                    }) {
-                        HStack {
-                            Text(showExampleContents ? "예시닫기" : "예시보기")
-                            Image(systemName: showExampleContents ? "chevron.up" : "chevron.down")
-                        }
-                        .foregroundColor(Color(hex:"898A8D"))
+        VStack(alignment: .leading) {
+            LSWideCardView(speakingStructure: speakingStructure)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    ForEach(0..<speakingStructure.components.count, id: \.self) { index in
+                        FlowComponentView(speakingStructure: speakingStructure, index: index, showExampleContents: $showExampleContents)
                     }
                 }
-                
-                Spacer()
-                
-                // 퀴즈로 넘어가는 버튼
-                NavigationLink(destination: StructureQuizView()) {
-                    RoundedRectangle(cornerRadius: 18)
-                        .foregroundStyle(Color(hex: "51D7A7"))
-                        .frame(width: 353, height: 54)
-                        .overlay(
-                            Text("흐름 학습하기")
-                                .fontWeight(.bold)
-                                .foregroundStyle(Color.white)
-                        )
+                .padding()
+                // 예시문장 열고 닫는 버튼
+                Button(action: {
+                    withAnimation {
+                        showExampleContents.toggle()
+                    }
+                }) {
+                    HStack {
+                        Text(showExampleContents ? "예시닫기" : "예시보기")
+                        Image(systemName: showExampleContents ? "chevron.up" : "chevron.down")
+                    }
+                    .foregroundColor(Color(hex:"898A8D"))
                 }
             }
-            .padding()
-            .background(Color(hex: "F1F3F6"))
+            
+            Spacer()
+            
+            // 퀴즈로 넘어가는 버튼
+            NavigationLink(destination: StructureQuizView()) {
+                RoundedRectangle(cornerRadius: 18)
+                    .foregroundStyle(Color(hex: "51D7A7"))
+                    .frame(width: 353, height: 54)
+                    .overlay(
+                        Text("흐름 학습하기")
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                    )
+            }
         }
+        .padding()
+        .background(Color(hex: "F1F3F6"))
     }
     
     func getTextHeight(for text: String, in width: CGFloat) -> CGFloat {
@@ -69,5 +67,7 @@ struct StructureFlowView: View {
 
 
 #Preview {
-    StructureFlowView(speakingStructure: .prep)
+    NavigationStack {
+        StructureFlowView(speakingStructure: .prep)
+    }
 }
