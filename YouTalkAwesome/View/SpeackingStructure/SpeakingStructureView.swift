c834c347e5ed8a23c71e3f85a1f436470bbc83be
norang <norang1221@gmail.com>
2024-11-23 21:08:25 +0900

    상황별 말하기 화면 옮김

--- YouTalkAwesome/View/SpeackingStructure/SpeakingStructureView.swift
+++ YouTalkAwesome/View/SpeackingStructure/SpeakingStructureView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SpeakingStructureView: View {
     @EnvironmentObject private var coordinator: AppCoordinator
-    
+
+    let preparedSituation = [
+        "나에게 모두 이목집중 👀","한 방 터뜨리기 👊🏻","날카로운 상황 분석 📈","내 잠재력 어필하기 💫", "깔끔 요약정리 ✍🏻", "내 말에 설득당할 걸? 😎", "멋진 성장 스토리 🍀","현재 문제점 진단하기 🦔", "나의 경험 어필하기 🤓", "해결책 제시하기 💡", "문제해결 능력 어필⚡️", "생생하게 상황 묘사 🌼"
+    ]
+
     var body: some View {
         ScrollView {
             VStack(spacing: 10){
@@ -38,6 +42,38 @@ struct SpeakingStructureView: View {
             .navigationBarBackButtonHidden()
         }
         .scrollIndicators(.hidden)
+    }
+    
+    @ViewBuilder
+    func makeSituationCard(_ situation: String) -> some View {
+        Text(situation)
+            .customFont(.body3_light)
+            .frame(height: 16)
+            .background {
+                GeometryReader { geometry in
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.gray6)
+                        .frame(width: geometry.size.width + 15, height: 44)
+                        .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
+                }
+            }
+            .onTapGesture {
+            }
+    }
+    
+    private func evaluateSituation(text: String) -> SpeakingStructure {
+        switch text {
+        case "깔끔 요약정리 ✍🏻", "내 말에 설득당할 걸? 😎":
+            return .prep
+        case "나의 경험 어필하기 🤓", "멋진 성장 스토리 🍀":
+            return .star
+        case "내 잠재력 어필하기 💫", "날카로운 상황 분석 📈":
+            return .grow
+        case "한 방 터뜨리기 👊🏻", "나에게 모두 이목집중 👀":
+            return .aida
+        default:
+            return .psb
+        }
     }
 }
 
